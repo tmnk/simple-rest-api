c4991830b2bef0c670cebb74051740f1969f06ea
tmnk <temnikovgiga@yandex.ru>
2022-08-17 23:16:42 +0300

    feat: add configs

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/BurntSushi/toml"
+	"github.com/tmnk/simple-rest-api/internal/app/apiserver"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(&configPath, "config-path", "configs/apiserver.toml", "path to config file")
+}
 
 func main() {
+	flag.Parse()
+	config := apiserver.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := apiserver.New(config)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println("Hello World")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tmnk/simple-rest-api
 
 go 1.17
+
+require github.com/BurntSushi/toml v1.2.0 // indirect
